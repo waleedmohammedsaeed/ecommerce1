2c08c1269e507321cfff052cd047c1b0f457a03b
@waleedmohammedsaeed <waleed_mohammed2008@hotmail.com>
2024-01-31 13:37:06 +0200

    commit 2

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Sector(models.Model):
+    sectorName = models.CharField(max_length=50),
+    description = models.TextField()
+
+
+    def __str__(self):
+        return self.sectorName
+
+    class Meta:
+        verbose_name = 'sector'
+        verbose_name_plural = 'sectors'
